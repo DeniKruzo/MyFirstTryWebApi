2c5b4dd556b13205a95ac07c562de96006dd1578
DeniKruzo <kruzodanil@mail.ru>
2023-10-09 17:22:48 +0500

    ASP6ANDCRUD

---
+++ Notes.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Notes.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[contoller]/[action]")]
+    public abstract class BaseController : ControllerBase
+    {
+        //Для формирования команд 
+        private IMediator _mediator;
+        protected IMediator Mediator => 
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        internal Guid UserId => !User.Identity.IsAuthenticated
+          ? Guid.Empty
+          : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+    }
+}
--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,6 +1,64 @@
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Application;
+using Notes.Persistence;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddControllers();
+builder.Services.AddPersistence(builder.Configuration);
+
+// ¬ реальном проекте так не делать - это дает доступ другим сайтам к нашему.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+
+    }
+
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors("AllowAll");
+app.MapControllers();
 
 app.Run();
