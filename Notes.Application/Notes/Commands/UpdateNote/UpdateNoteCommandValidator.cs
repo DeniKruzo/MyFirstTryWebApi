9c013739f86e32320a43368cf7a84d061dd9e7d7
DeniKruzo <kruzodanil@mail.ru>
2023-10-10 18:23:33 +0500

    AddFluentV

---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandValidator : AbstractValidator<UpdateNoteCommand>
+    {
+        public UpdateNoteCommandValidator() 
+        {
+            RuleFor(updateNoteCommand => updateNoteCommand.UserId).NotEqual(Guid.Empty);
+            RuleFor(updateNoteCommand => updateNoteCommand.Id).NotEqual(Guid.Empty);
+            RuleFor(updateNoteCommand => updateNoteCommand.Title)
+                .NotEmpty().MaximumLength(250);
+        }
+    }
+}
